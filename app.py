3027dab6815224ca3b2c2cb0f9457022e1f9774f
ChieuAnh-Nguyen <chieuanh.nguyen@sjsu.edu>
2022-09-01 20:03:20 -0700

    omgit works

--- app.py
+++ app.py
@@ -9,27 +9,23 @@ app = Flask(__name__)
 
 def home():
     if request.method == 'POST':
-        text_file = open('file','r')
-        text = text_file.read()
-        soup = BeautifulSoup(text).get_text()
-        print("it posted in home")
-        return render_template("result.html",soup = soup)
+        return render_template("result.html")
     else:
+        print("it get in home")
+
         return render_template("layout.html")
 
 
 @app.route("/result", methods = ['GET', 'POST'])
 
 def result():
-    print('hello')
     if request.method == 'POST':
-        text_file = open('file','r')
-        text = text_file.read()
+        text = request.files['file']
         soup = BeautifulSoup(text).get_text()
-        print("it posted in results")
-        return render_template("result.html",soup = soup)
+        print("it posted in request")
+        return render_template("result.html", soup = soup)
     else:
-        print('poop')
+        print('it get in request')
 
 
         
